5a15ce35e386199ff5e09148d728b1d384bb925a
thiagoarce <thiagoarce@outlook.com>
2020-02-18 18:57:07 -0400

    sprites automatizados -correção bb necessária

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
 css_dir = "css"
 sass_dir = "css"
 images_dir = "imagens"
 javascripts_dir = "js"
+http_path = "../"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
